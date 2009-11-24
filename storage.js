3a7ca01bb9a0f727ac48a758d6f629b4342002e7
Brett Wejrowski <brett@thewojogroup.com>
2009-11-24 18:48:36 -0500

    Cross browser tested with some IE.

--- storage.js
+++ storage.js
@@ -1,21 +1,22 @@
 (function(){
-	if( !!localStorage && !!sessionStorage ){
-		document["save"] = function(name, value){
+	var window = this;
+	if( typeof localStorage != 'undefined' && typeof sessionStorage != 'undefined' ){
+		window["save"] = function(name, value){
 			localStorage[name] = value;
-		}
-		document["read"] = function(name){
+		};
+		window["read"] = function(name){
 			return localStorage[name];
-		}
-	} else if(!!document.cookie){
-		var expiresAt = 30/*days*/*24/*hours*/*60/*minutes*/*60/*seconds*/,
+		};
+	} else if( navigator.cookieEnabled ){
+		var expiresAt = 30*24*60*60,
 			maxCookieSize = 4000;
-		function createCookie(name,value) {
+		var createCookie = function(name,value) {
 			var date = new Date();
 			date.setTime(date.getTime() + expiresAt);
 			var expires = "; expires=" + date.toGMTString();
 			document.cookie = name+"="+value+expires+"; path=/";
-		}
-		function readCookie(name) {
+		};
+		var readCookie = function(name) {
 			var nameEQ = name + "=";
 			var ca = document.cookie.split(';');
 			for(var i=0,iLen=ca.length;i<iLen;i++) {
@@ -24,13 +25,13 @@
 				if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
 			}
 			return null;
-		}
-		function eraseCookie(name) {createCookie(name,"",-1);}
-		document["save"] = function(name,value){
+		};
+		var eraseCookie = function(name) {createCookie(name,"",-1);};
+		window["save"] = function(name,value){
 			var data="",x=0,xlen,exists=false;
-			while(readCookie("storageData_"+x) !== null)
+			while(readCookie("storageData_"+x) !== null){
 				data+=readCookie("storageData_"+x++);
-			
+			}
 			data = data.split('++');
 			xlen=data.length;
 			for(x=0;x<xlen;x++){
@@ -54,8 +55,8 @@
 					eraseCookie("storageData_"+x);
 				x++;
 			}
-		}
-		document["read"] = function(name){
+		};
+		window["read"] = function(name){
 			var data="",x=0,xlen,exists=false;
 			while(readCookie("storageData_"+x) !== null)
 				data+=readCookie("storageData_"+x++);
@@ -69,7 +70,7 @@
 				}
 			}
 			return null;
-		}
+		};
 	} else {
 		try{
 			console.log("No local storage supported");
